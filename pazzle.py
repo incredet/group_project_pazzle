f81ed4237bfeb168997184e588f302069ead2b5f
olenahnyp <hnyp.pn@ucu.edu.ua>
2023-02-08 11:30:03 +0200

    Validate row

--- pazzle.py
+++ pazzle.py
@@ -6,11 +6,10 @@ def validate_row(board):
     >>> validate_row(board = ["****1****", "***11****", "**123****", "*1234****", "123456789", "12345678*", "1234567**", "123456***", "12345****"])
     False
     >>> validate_row(board = ["**** ****", "***1 ****", "**  3****", "* 4 1****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    False
     """
     for row in board:
-        if ' ' in row:
-            return False
-        for index, element in enumerate(row[:-1]):
-            if element != '*' and element == row[index + 1]:
+        for element in row:
+            if element != '*' and element != ' ' and row.count(element) > 1:
                 return False
     return True
