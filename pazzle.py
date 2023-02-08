d4e8e527d8fb12c2bd218530233d4bcec2301ff0
olenahnyp <hnyp.pn@ucu.edu.ua>
2023-02-08 11:22:49 +0200

    Validate row

--- pazzle.py
+++ pazzle.py
@@ -1,1 +1,16 @@
-print("hello world")
+def validate_row(board):
+    """
+    Return True if there are no equal numbers in a row else False.
+    >>> validate_row(board = ["****1****", "***12****", "**123****", "*1234****", "123456789", "12345678*", "1234567**", "123456***", "12345****"])
+    True
+    >>> validate_row(board = ["****1****", "***11****", "**123****", "*1234****", "123456789", "12345678*", "1234567**", "123456***", "12345****"])
+    False
+    >>> validate_row(board = ["**** ****", "***1 ****", "**  3****", "* 4 1****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    """
+    for row in board:
+        if ' ' in row:
+            return False
+        for index, element in enumerate(row[:-1]):
+            if element != '*' and element == row[index + 1]:
+                return False
+    return True
